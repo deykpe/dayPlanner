ef59d75c5f4f8ada13731c52ebd074f46813430a
David Pang <dpang2@uci.edu>
2019-11-04 18:05:50 -0800

    Local Storage Finished

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
 var currentdate = new Date().toLocaleDateString()
-$("#currentdate").append(currentdate) 
+$("#currentdate").append(currentdate)
 
+$(".saveBtn").on("click", function(){
+    var value = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time,value)
+    console.log(value, time)
+})
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+ 
+  // get current number of hours
+  var currentHour = moment().hours();
+
+  // loop over time blocks
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    // check if we've moved past this time
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } 
+    else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } 
+    else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+
+
+//hourUpdater();
